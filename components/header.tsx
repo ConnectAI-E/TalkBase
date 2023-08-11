863ad448187420518015b12f3be7dbcb8bd53434
river <laolei@forkway.cn>
2023-08-11 12:28:52 +0800

    feat: improve the copywriting

--- components/header.tsx
+++ components/header.tsx
@@ -4,19 +4,16 @@ import {githubUrl} from '../constant';
 import {useTranslation} from 'next-i18next';
 
 Header.propTypes = {
-
 };
 
 function Header() {
 
     const { t, i18n } = useTranslation('common');
-
     return (
         <div
             className="flex flex-col space-y-1 p-4 border sm:p-10 text-sm px-10 select-none">
             <h1 className="text-sm font-semibold text-black">
-                Welcome to TalkBase!
-
+                {t('welcome')} TalkBase!
             </h1>
             <p className="text-gray-500">
                 {t('thisisan')}{ ' ' }
@@ -57,7 +54,7 @@ function Header() {
                     className="font-medium underline underline-offset-4 transition-colors hover:text-black"
                 >
                     ConnectAI
-                </a>{ ' ' } {t('with')} ❤️ .
+                </a>{ ' ' } {t('with')} .
 
             </p>
         </div>
